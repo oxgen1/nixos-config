6de388bd9a4ae7f3bd109a09bc3b846930180c72
Jack <j@jack.farm>
2024-09-22 12:58:03 -0400

    direnv enabled

--- home/vscode/vscode.nix
+++ home/vscode/vscode.nix
@@ -10,6 +10,8 @@
         enable = true;
         extensions = with pkgs; [
             vscode-extensions.vue.volar
+            vscode-extensions.ms-vscode-remote.remote-ssh
+            vscode-extensions.ms-vscode-remote.remote-ssh-edit
             vscode-extensions.rust-lang.rust-analyzer
             vscode-extensions.jnoortheen.nix-ide
             ];
--- laptop/configuration.nix
+++ laptop/configuration.nix
@@ -153,6 +153,7 @@
     usbutils
     yubikey-manager
     gnupg
+    bun
   ];
   environment.sessionVariables.NIXOS_OZONE_WL = "1";
 
